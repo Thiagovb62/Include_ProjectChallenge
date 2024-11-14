d5324975b32fff82d601d6dcb37bfb9207f2da7d
thiago.bezerra <thiago.bezerra@dataprev.gov.br>
2024-11-14 20:57:54 -0300

    fix toglleNavlist function

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
+menu.addEventListener('click', toggleNavList);
+
 function toggleNavList() {
     if (window.innerWidth < 950) {
-        navlist.style.display = navlist.style.display === 'none' ? 'flex' : 'none';
+        navlist.style.display = 'none';
         menu.classList.toggle('bx-x');
-        navlist.classList.toggle('open') ? navlist.style.display = 'flex' : navlist.style.display = 'none';
+        navlist.classList.toggle('open');
+        navlist.style.display = navlist.classList.contains('open') ? 'flex' : 'none';
         navlist.style.backdropFilter = navlist.classList.contains('open') ? 'blur(32px)' : 'none';
     }
 }
 
-menu.onclick = toggleNavList;
-
-window.onresize = () => {
-    if (window.innerWidth >= 950) {
+function closeNavList() {
+    if (window.innerWidth < 950) {
+        menu.classList.remove('bx-x');
         navlist.classList.remove('open');
-        navlist.style.backdropFilter = 'none';
         navlist.style.display = 'none';
+        navlist.style.backdropFilter = 'none';
+    } else {
+        navlist.style.display = 'flex';
+        navlist.style.backdropFilter = 'none';
     }
 }
+
+window.addEventListener('resize', closeNavList);
