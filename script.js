040842c2b9f586618cfbc80c1fd82c38f96ffec6
thiago.bezerra <thiago.bezerra@dataprev.gov.br>
2024-11-14 19:34:19 -0300

    fix error in navlist toggle control

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let navlist = document.querySelector('.navlist');
 navlist.style.display = navlist.style.display === 'none' ? 'flex' : 'none';
 function toggleNavList() {
     if (window.innerWidth < 950) {
+        navlist.style.display = navlist.style.display === 'none' ? 'flex' : 'none';
         menu.classList.toggle('bx-x');
         navlist.classList.toggle('open') ? navlist.style.display = 'flex' : navlist.style.display = 'none';
         navlist.style.backdropFilter = navlist.classList.contains('open') ? 'blur(32px)' : 'none';
